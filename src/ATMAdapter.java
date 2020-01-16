324b8268d6930cbc1655e560a407fe6d38e11e1e
qoberson <oberson.quentin@hotmail.fr>
2020-01-16 09:31:21 +0100

    Ajout de insertPin

--- src/ATMAdapter.java
+++ src/ATMAdapter.java
@@ -15,6 +15,7 @@ public class ATMAdapter {
         Card c = new Card(1234, new Account(100));
         System.out.println("Inserted card");
         int val = sut.insertCard(c);
+        assert val == 0;
     }
 
     public void insertCardBlocked() {
@@ -25,6 +26,7 @@ public class ATMAdapter {
         c.checkPin(1111);
         c.checkPin(1111);
         int val = sut.insertCard(c);
+        assert val == -2;
     }
 
     public void cancel() {
@@ -32,8 +34,66 @@ public class ATMAdapter {
         sut.cancel();
     }
 
-    public void chooseAmount(){
+    public void chooseAmountValid(){
 
+    }
+
+    public void chooseAmountNoValid(){
+
+    }
+
+    public void inputPinValid(){
+        System.out.println("InputPin");
+        sut.inputPin(1234);
+    }
+
+    public void inputPinNoValid(){
+        System.out.println("InputPin");
+        sut.inputPin(1111);
+    }
+
+    public void inputPinBlocked(){
+        System.out.println("InputPin");
+        int val = 0;
+        do {
+            val = sut.inputPin(1111);
+        }while (val != -3);
+    }
+
+    public void takeBills() {
+        System.out.println("Taking bills");
+        int[] bills = sut.takeBills();
+        assert bills != null;
+    }
+
+    public void wait6SecondsTakingBills() {
+        System.out.println("Wait 6s on taking bills");
+        try {
+            Thread.sleep(6001);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int[] bills = sut.takeBills();
+        assert bills == null;
+    }
+
+    public void takeCard() {
+        System.out.println("Taking card");
+        Card card = sut.takeCard();
+        assert card != null;
+    }
+
+    public void wait6SecondsTakingCard() {
+        System.out.println("Wait 6s on taking card");
+        try {
+            Thread.sleep(6001);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Card card = sut.takeCard();
+        assert card == null;
     }
 
 }
